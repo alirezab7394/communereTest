449be97a501b1c5189e2188c133e4fe281af4646
alirezab7394 <alireza7394@gmail.com>
2021-08-08 00:58:54 +0430

    feature edit popup

---
+++ src/components/Map/components/LocationPopup/index.tsx
@@ -0,0 +1,23 @@
+import { LatLngLiteral } from "leaflet";
+import { Popup } from "react-leaflet";
+import Button from "src/components/Widgets/Button";
+import { LocationProps } from "src/interfaces/Location";
+import "./styles.scss";
+
+interface Props extends LocationProps {
+  onEdit?: (value: LocationProps) => void;
+}
+export default function LocationPopup({ type, logo, position, title, id, onEdit = () => {} }: Props) {
+  return (
+    <Popup>
+      <div className="locationPopup">
+        <div className="locationPopup__title">{title}</div>
+        <div>
+          <Button className="locationPopup__btn" onClick={() => onEdit({ id, position, type, title, logo })}>
+            Edit
+          </Button>
+        </div>
+      </div>
+    </Popup>
+  );
+}
